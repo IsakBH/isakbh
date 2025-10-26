5ffbe0781b7e4638b4360c41f0252919fc4feed5
IsakBH <isak@brunhenriksen.net>
2025-10-26 18:15:56 +0100

    oppdater vil

--- assets/samtalerpanettutvikling.sql
+++ assets/samtalerpanettutvikling.sql
@@ -1,33 +1,29 @@
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: May 25, 2025 at 09:04 PM
--- Server version: 11.7.2-MariaDB
--- PHP Version: 8.4.7
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `conversationWeb`
---
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dm_conversations` (
+  `id` int(11) NOT NULL,
+  `user1_id` int(11) NOT NULL,
+  `user2_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Table structure for table `users`
---
+CREATE TABLE IF NOT EXISTS `dm_messages` (
+  `id` int(11) NOT NULL,
+  `conversation_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `to_user_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `sent_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `mail` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -39,33 +35,27 @@ CREATE TABLE `users` (
   `password_reset_token` varchar(64) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
 
-INSERT INTO `users` (`id`, `mail`, `username`, `password`, `register_date`, `profile_picture`, `email_verification_token`, `email_verified`, `password_reset_token`) VALUES
-(5, 'IkkeStjelMin@Email.com', 'apekatt', 'Et Hashet Passord', '2025-05-23 20:28:45', 0x64656661756c742e706e67, NULL, 1, NULL)
-;
+ALTER TABLE `dm_conversations`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_user_pair` (`user1_id`,`user2_id`),
+  ADD UNIQUE KEY `unique_conversation` (`user1_id`,`user2_id`);
 
---
--- Indexes for dumped tables
---
+ALTER TABLE `dm_messages`
+  ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `users`
---
+ALTER TABLE `dm_conversations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `dm_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
